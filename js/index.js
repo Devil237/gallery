877d2a56786a0ffb56089faca5db37fc432cb248
dmitriy <blade237@i.ua>
2015-12-23 01:24:15 +0200

    creation of the project gallery

---
+++ js/index.js
@@ -0,0 +1,12 @@
+﻿document.getElementById( 'gallery' ).addEventListener( 'click', function (elem) {
+
+    if (elem.target.tagName === 'IMG') {
+        var howMany = this.querySelectorAll( 'li' ).length;
+		var parentElement = elem.target.parentNode;
+        this.removeChild(parentElement);        
+    }
+
+}, false);
+
+
+
